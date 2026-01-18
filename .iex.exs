95c760a163957b54dd236973e89c8b66ebe01738
Alexander Mandrikov <mandrikov@pm.me>
2026-01-18 14:43:48 +0700

    add portfolio alias to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -3,6 +3,7 @@ IEx.configure(inspect: [limit: :infinity])
 import_if_available(Ecto.Query)
 
 alias Crypo.Repo
+alias Crypo.Portfolio
 alias Crypo.Prices
 alias Crypo.Prices.Price
 alias Crypo.Settings
